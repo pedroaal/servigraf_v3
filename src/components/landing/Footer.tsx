73dff3d86935511f4989f3dac961b6e54af39374
pedroaal <pedroaal04@gmail.com>
2025-12-14 08:32:01 -0500

    minor fix

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -1,18 +1,14 @@
-import {
-	FaBrandsWhatsapp,
-	FaSolidEnvelope,
-	FaSolidPhone,
-} from "solid-icons/fa";
+import { FaBrandsWhatsapp, FaSolidEnvelope } from "solid-icons/fa";
 
 const Footer = () => {
 	return (
 		<footer class="footer footer-center p-4 bg-primary text-primary-content">
 			<div class="flex flex-wrap items-center justify-between w-full max-w-6xl gap-4">
-				<div class="font-bold text-lg">ServiGraf</div>
+				<div class="font-bold text-lg">Grafos</div>
 				<div class="flex flex-wrap gap-4">
 					<span class="flex items-center gap-2">
-						<FaSolidPhone />
-						514 3236
+						<FaBrandsWhatsapp />
+						098 762 0452
 					</span>
 					<span class="flex items-center gap-2">
 						<FaBrandsWhatsapp />
@@ -20,7 +16,7 @@ const Footer = () => {
 					</span>
 					<span class="flex items-center gap-2">
 						<FaSolidEnvelope />
-						info@servigraf.me
+						servigraf.ec@gmail.me
 					</span>
 				</div>
 			</div>
